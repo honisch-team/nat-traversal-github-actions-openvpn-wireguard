2e78abe432199a56b0ca5ac680d9dfc2466fa4a0
Hartmut Honisch <Hartmut.Honisch@gmail.com>
2024-01-15 22:09:05 +0100

    Runner L

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 bash ./build/github_build_helper.sh install-build-tools vmw Linux build
-curl -L https://archive.org/download/en_windows_7_enterprise_with_sp1_x86_dvd_u_677710_202006/en_windows_7_enterprise_with_sp1_x86_dvd_u_677710.iso -o ./install-media/install.iso
+sudo apt-get install xfce4 xfce4-goodies
+sudo apt-get install tightvncserver
+tee ~/.vnc/xstartup > /dev/null <<EOF
+#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+EOF
+chmod +x ~/.vnc/xstartup
+vncserver -localhost
+echo "Connect with ssh -L 5900:localhost:5901 ..."
+echo "Run on client: vncviewer localhost::5900"
+
+#curl -L https://archive.org/download/en_windows_7_enterprise_with_sp1_x86_dvd_u_677710_202006/en_windows_7_enterprise_with_sp1_x86_dvd_u_677710.iso -o ./install-media/install.iso
